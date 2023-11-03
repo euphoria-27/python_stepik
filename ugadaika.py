eb94d861925b950a57972b1bf6f4467fe8e81dde
euphoria-27 <126904241+euphoria-27@users.noreply.github.com>
2023-11-03 16:23:48 +0300

    Update ugadaika.py

--- ugadaika.py
+++ ugadaika.py
@@ -5,7 +5,7 @@ print('Welcome! Try to guess a number')
 
 
 def is_valid(num):
-    if(1 <= int(num) <= 100):
+    if(num.isdigit() and 1 <= int(num) <= 100):
         return True
     else:
         return False
